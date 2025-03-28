b1ac872b357a8f5cdaee24a2d2ce013ed9b799de
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-03-28 14:44:47 -0500

    modify controller to fetch BTC price from Binance.us and coinbase

--- Controllers/PriceController.cs
+++ Controllers/PriceController.cs
@@ -15,24 +15,52 @@ namespace ArbitrageApp.Controllers
         {
             try
             {
-                HttpResponseMessage response = await _client.GetAsync("https://api.binance.us/api/v3/ticker/price?symbol=BTCUSDT");
-                response.EnsureSuccessStatusCode();
+                var binancePrice = await GetBinancePrice();
 
-                string responseBody = await response.Content.ReadAsStringAsync();
-                JsonDocument jsonDoc = JsonDocument.Parse(responseBody);
+                var coinbasePrice = await GetCoinbasePrice();
 
-                var coinPrice = new CoinPriceModel
+                var response = new
                 {
-                    Symbol = jsonDoc.RootElement.GetProperty("symbol").GetString(),
-                    Price = jsonDoc.RootElement.GetProperty("price").GetString()
+                    BinancePrice = binancePrice,
+                    CoinbasePrice = coinbasePrice
                 };
 
-                return Ok(coinPrice);
+                return Ok(response);
             }
             catch (Exception ex)
             {
                 return Problem($"Error fetching data: {ex.Message}");
             }
+        }
+
+        private async Task<CoinPriceModel> GetBinancePrice()
+        {
+            HttpResponseMessage response = await _client.GetAsync("https://api.binance.us/api/v3/ticker/price?symbol=BTCUSDT");
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+            JsonDocument jsonDoc = JsonDocument.Parse(responseBody);
+
+            return new CoinPriceModel
+            {
+                Symbol = jsonDoc.RootElement.GetProperty("symbol").GetString() ?? string.Empty,
+                Price = jsonDoc.RootElement.GetProperty("price").GetString() ?? string.Empty
+            };
+        }
+
+        private async Task<CoinPriceModel> GetCoinbasePrice()
+        {
+            HttpResponseMessage response = await _client.GetAsync("https://api.coinbase.com/v2/prices/spot?currency=USD");
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+            JsonDocument jsonDoc = JsonDocument.Parse(responseBody);
+
+            return new CoinPriceModel
+            {
+                Symbol = "BTCUSD", // Coinbase gives price in USD for BTC, so we can just set this manually
+                Price = jsonDoc.RootElement.GetProperty("data").GetProperty("amount").GetString() ?? string.Empty
+            };
         }
     }
 }
