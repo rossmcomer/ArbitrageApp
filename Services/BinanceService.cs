7e201c781e812cfb5400f952b97d3f68569e9dd6
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-03-28 17:07:21 -0500

    cache jsonSerializerOptions in BinanceService to reduce memory usage and improve performance

--- Services/BinanceService.cs
+++ Services/BinanceService.cs
@@ -5,6 +5,11 @@ public class BinanceService
 {
     private readonly HttpClient _client;
 
+    private static readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = true
+    };
+
     public BinanceService()
     {
         _client = new HttpClient();
@@ -12,19 +17,13 @@ public class BinanceService
 
     public async Task<List<CoinPriceModel>> GetAllTickers()
     {
-        // Fetch all tickers from Binance.US
         HttpResponseMessage response = await _client.GetAsync("https://api.binance.us/api/v3/ticker/price");
         
         if (response.IsSuccessStatusCode)
         {
             string responseBody = await response.Content.ReadAsStringAsync();
-
-            var options = new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true
-            };
             
-            var tickers = JsonSerializer.Deserialize<List<CoinPriceModel>>(responseBody, options)?? new List<CoinPriceModel>();
+            var tickers = JsonSerializer.Deserialize<List<CoinPriceModel>>(responseBody, _jsonOptions) ?? [];
 
             
             return tickers;   
@@ -32,7 +31,6 @@ public class BinanceService
         }
         else
         {
-            // If the request fails, you can throw an exception or return an empty list.
             throw new HttpRequestException($"Error fetching tickers: {response.StatusCode}");
         }
     }
