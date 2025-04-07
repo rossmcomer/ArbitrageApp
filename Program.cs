9cb1e2eb612f3541696c792959b72071412c4739
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-04-07 13:58:46 -0500

    register SQL Server in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using ArbitrageApp.Services;
+using ArbitrageApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,9 @@ builder.Services.AddSingleton<CryptoComService>();
 builder.Services.AddSingleton<KrakenService>();
 builder.Services.AddHttpClient();
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
